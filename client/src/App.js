e4c733fada3f3c22061fc5c8979b5c2c04578a27
linszay <lindseyjlancaster@gmail.com>
2023-11-21 13:37:51 -0600

    fixed client folder issues

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+/*import ApolloClient from 'apollo-boost';
+/*
+...
+*/
+// components
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import TaskList from './components/TaskList';
+import AddTask from './components/AddTask';
+import AddProject from './components/AddProject';
+// apollo client setup
+/*
+...
+*/
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>GraphQL App</h1>
+        <TaskList />
+        <AddProject />
+        <AddTask />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
